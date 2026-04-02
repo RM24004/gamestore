781527c8680a50664f4385bc6c450356602db978
RM24004 <167376455+RM24004@users.noreply.github.com>
2026-04-01 20:30:18 -0600

    Base frontend
    base del frontend subida con todas sus dependecias y react

---
+++ frontend/gamerstore-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from './assets/vite.svg'
+//import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>GamerStore</h1>
+      <p>Bienvenido al sistema</p>
+      <p>hola mundo soy victor</p>
+    </div>
+  );
+}
+
+export default App
